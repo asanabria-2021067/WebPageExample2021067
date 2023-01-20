d5d5e5eb4de8e480a136241886af2f159d1476ff
Angel Sanabria <asanabria-2021067@kinal.edu.gt>
2023-01-20 08:59:40 -0600

    Primer commit v1 webpage

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+
+function saludar(){
+    console.log('Hola Mundo');
+}
+
+function sumar(){
+    console.log("Saludos")
+    let numero1 = (document.getElementById('numero1')).value;
+    let numero2 = (document.getElementById('numero2')).value;
+    let resultado= parseInt(numero1) +parseInt(numero2);
+    console.log(resultado);
+}
